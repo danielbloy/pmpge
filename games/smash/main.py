dcd2f29e9b0f024f736e40e06152ecbbeaee6618
Daniel Bloy <daniel_bloy@hotmail.com>
2026-02-22 17:40:15 +0000

    Added initial assets

---
+++ games/smash/main.py
@@ -0,0 +1,27 @@
+import os
+
+os.environ['SDL_VIDEO_WINDOW_POS'] = f'700,100'
+
+import pgzrun
+from pgzge.core import draw_game, update_game
+from pgzero.clock import Clock
+from pgzero.keyboard import Keyboard
+from pgzero.screen import Screen
+
+screen: Screen
+keyboard: Keyboard
+clock: Clock
+
+WIDTH = 600
+HEIGHT = 640
+
+
+def draw():
+    draw_game(screen.surface)
+
+
+def update(dt):
+    update_game(dt)
+
+
+pgzrun.go()

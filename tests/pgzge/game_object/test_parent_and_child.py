a29c092ab990e3a34b7867b25380ff6701f5d134
Daniel Bloy <daniel_bloy@hotmail.com>
2026-03-16 21:34:39 +0000

    Added more tests

--- tests/pgzge/game_object/test_parent_and_child.py
+++ tests/pgzge/game_object/test_parent_and_child.py
@@ -1,1 +1,10 @@
 # TODO: Test all of the more complex parent and child checks.Validate the parent passed in works.
+
+
+# TODO: test propagation of active property to children
+# TODO: Test reset propagates to children
+# TODO: Test destroy propagates to children
+# TODO: Test add_child()
+# TODO: Test remove_child()
+# TODO: Test draw_hierarchy()
+# TODO: Test update_hierarchy()
--- tests/pgzge/game_object/test_properties.py
+++ tests/pgzge/game_object/test_properties.py
@@ -1,1 +1,84 @@
-# TODO: Validate the state properties such as active and destroyed work correctly..
+from pgzge.core import GameObject
+from tests.pgzge.game_object.test_utilities import TestHandlers
+
+
+class TestProperties:
+    """
+    This suite of tests validates the GameObject properties. the tests are all relatively
+    simple and cover the basic functionality of the properties.
+    """
+
+    def test_name_property(self):
+        """
+        Tests the name property which is read-only and can only be set in the constructor.
+        """
+        go = GameObject()
+        assert go.name is None
+
+        go = GameObject(name="test")
+        assert go.name == "test"
+
+        go = GameObject(name="test.name")
+        assert go.name == "test.name"
+
+    def test_active_property(self):
+        """
+        Tests the active property which is mutable and will trigger the activate and deactivate
+        handlers. This does not test propagation of active to children, that is handled in the
+        test_parent_and_child.py.
+        """
+        handlers = TestHandlers()
+        go = GameObject(
+            activate_handler=handlers.activate,
+            deactivate_handler=handlers.deactivate)
+
+        assert go.active is True
+        handlers.validate(activate=go, activate_count=1)
+
+        # Try setting it to true again, nothing should happen
+        handlers.reset()
+        go.active = True
+        assert go.active is True
+        handlers.validate()
+
+        handlers.reset()
+        go.active = False
+        assert go.active is False
+        handlers.validate(deactivate=go, deactivate_count=1)
+
+        # Try setting it to false again, nothing should happen
+        handlers.reset()
+        go.active = False
+        assert go.active is False
+        handlers.validate()
+
+        # Create a new GameObject, starting deactivated.
+        handlers = TestHandlers()
+        go = GameObject(
+            active=False,
+            activate_handler=handlers.activate,
+            deactivate_handler=handlers.deactivate)
+        assert go.active is False
+        handlers.validate(deactivate=go, deactivate_count=1)
+
+        handlers.reset()
+        go.active = True
+        assert go.active is True
+        handlers.validate(activate=go, activate_count=1)
+
+    def test_reset(self):
+        assert False
+
+    def test_destroyed(self):
+        """
+        Validates the destroyed property represents the destroyed state,
+        """
+        go = GameObject()
+        assert not go.destroyed
+
+        go.destroy()
+        assert go.destroyed
+        go.destroy()
+        assert go.destroyed
+
+    # TODO: test each of the add_handler() methods

e151a49d748aed176a829e1aa7caecb63c27b931
Daniel Bloy <daniel_bloy@hotmail.com>
2026-03-18 07:54:54 +0000

    Minor refactor to reduce indentation.

--- tests/pgzge/game_object/test_properties.py
+++ tests/pgzge/game_object/test_properties.py
@@ -1,102 +1,104 @@
 from pgzge.core import GameObject
 from tests.pgzge.game_object.test_utilities import TestHandlers
 
+"""
+This suite of tests validates the GameObject properties. the tests are all relatively
+simple and cover the basic functionality of the properties.
+"""
 
-class TestProperties:
+
+def test_name_property():
     """
-    This suite of tests validates the GameObject properties. the tests are all relatively
-    simple and cover the basic functionality of the properties.
+    Tests the name property which is read-only and can only be set in the constructor.
     """
+    go = GameObject()
+    assert go.name is None
 
-    def test_name_property(self):
-        """
-        Tests the name property which is read-only and can only be set in the constructor.
-        """
-        go = GameObject()
-        assert go.name is None
+    go = GameObject(name="test")
+    assert go.name == "test"
 
-        go = GameObject(name="test")
-        assert go.name == "test"
+    go = GameObject(name="test.name")
+    assert go.name == "test.name"
 
-        go = GameObject(name="test.name")
-        assert go.name == "test.name"
 
-    def test_active_property(self):
-        """
-        Tests the active property which is mutable and will trigger the activate and deactivate
-        handlers. This does not test propagation of active to children, that is handled in the
-        test_parent_and_child.py.
-        """
-        handlers = TestHandlers()
-        go = handlers.create_game_object()
+def test_active_property():
+    """
+    Tests the active property which is mutable and will trigger the activate and deactivate
+    handlers. This does not test propagation of active to children, that is handled in the
+    test_parent_and_child.py.
+    """
+    handlers = TestHandlers()
+    go = handlers.create_game_object()
 
-        assert go.active is True
-        handlers.validate(activate=go, activate_count=1)
+    assert go.active is True
+    handlers.validate(activate=go, activate_count=1)
 
-        # Try setting it to true again, nothing should happen
-        handlers.reset()
-        go.active = True
-        assert go.active is True
-        handlers.validate()
+    # Try setting it to true again, nothing should happen
+    handlers.reset()
+    go.active = True
+    assert go.active is True
+    handlers.validate()
 
-        handlers.reset()
-        go.active = False
-        assert go.active is False
-        handlers.validate(deactivate=go, deactivate_count=1)
+    handlers.reset()
+    go.active = False
+    assert go.active is False
+    handlers.validate(deactivate=go, deactivate_count=1)
 
-        # Try setting it to false again, nothing should happen
-        handlers.reset()
-        go.active = False
-        assert go.active is False
-        handlers.validate()
+    # Try setting it to false again, nothing should happen
+    handlers.reset()
+    go.active = False
+    assert go.active is False
+    handlers.validate()
 
-        # Create a new GameObject, starting deactivated.
-        handlers = TestHandlers()
-        go = GameObject(
-            active=False,
-            activate_handler=handlers.activate,
-            deactivate_handler=handlers.deactivate)
-        assert go.active is False
-        handlers.validate(deactivate=go, deactivate_count=1)
+    # Create a new GameObject, starting deactivated.
+    handlers = TestHandlers()
+    go = GameObject(
+        active=False,
+        activate_handler=handlers.activate,
+        deactivate_handler=handlers.deactivate)
+    assert go.active is False
+    handlers.validate(deactivate=go, deactivate_count=1)
 
-        handlers.reset()
-        go.active = True
-        assert go.active is True
-        handlers.validate(activate=go, activate_count=1)
+    handlers.reset()
+    go.active = True
+    assert go.active is True
+    handlers.validate(activate=go, activate_count=1)
 
-    def test_reset(self):
-        """
-        Validates that reset() works from both the active and inactive states.
-        """
-        handlers = TestHandlers()
-        go = handlers.create_game_object()
 
-        assert go.active is True
-        handlers.validate(activate=go, activate_count=1)
+def test_reset():
+    """
+    Validates that reset() works from both the active and inactive states.
+    """
+    handlers = TestHandlers()
+    go = handlers.create_game_object()
 
-        # Now reset, we should get deactivated and then reactivated.
-        handlers.reset()
-        go.reset()
-        assert go.active is True
-        handlers.validate(activate=go, activate_count=1, deactivate=go, deactivate_count=1,
-                          called_order=["deactivate", "activate"])
+    assert go.active is True
+    handlers.validate(activate=go, activate_count=1)
 
-        # Now deactivate and reset, we should get deactivated and then activated.
-        go.active = False
-        handlers.reset()
-        go.reset()
-        assert go.active is False
-        handlers.validate(activate=go, activate_count=1, deactivate=go, deactivate_count=1,
-                          called_order=["activate", "deactivate"])
+    # Now reset, we should get deactivated and then reactivated.
+    handlers.reset()
+    go.reset()
+    assert go.active is True
+    handlers.validate(activate=go, activate_count=1, deactivate=go, deactivate_count=1,
+                      called_order=["deactivate", "activate"])
 
-    def test_destroyed(self):
-        """
-        Validates the destroyed property represents the destroyed state,
-        """
-        go = GameObject()
-        assert not go.is_destroyed
+    # Now deactivate and reset, we should get deactivated and then activated.
+    go.active = False
+    handlers.reset()
+    go.reset()
+    assert go.active is False
+    handlers.validate(activate=go, activate_count=1, deactivate=go, deactivate_count=1,
+                      called_order=["activate", "deactivate"])
 
-        go.destroy()
-        assert go.is_destroyed
-        go.destroy()
-        assert go.is_destroyed
+
+def test_destroyed():
+    """
+    Validates the destroyed property represents the destroyed state,
+    """
+    go = GameObject()
+    assert not go.is_destroyed
+
+    go.destroy()
+    assert go.is_destroyed
+    go.destroy()
+    assert go.is_destroyed
--- tests/pgzge/game_object/test_subclassing.py
+++ tests/pgzge/game_object/test_subclassing.py
@@ -1,6 +1,11 @@
 from pgzge.core import GameObject
 from tests.pgzge.game_object.test_utilities import TestHandlers
 
+"""
+This suite of tests validates that the main overridable placeholder methods are
+called at the expected points.
+"""
+
 
 class GameObjectSubclass(GameObject):
     """
@@ -30,79 +35,78 @@ class GameObjectSubclass(GameObject):
         return self._handlers
 
 
-class TestSubclassing:
+def test_activated_in_constructor():
     """
-    This suite of tests validates that the main overridable placeholder methods are
-    called at the expected points.
+    Validates that activated is called during construction.
     """
+    go = GameObjectSubclass()
+    go.handlers.validate(activate=go, activate_count=1)
 
-    def test_activated_in_constructor(self):
-        """
-        Validates that activated is called during construction.
-        """
-        go = GameObjectSubclass()
-        go.handlers.validate(activate=go, activate_count=1)
 
-    def test_deactivated_in_constructor(self):
-        """
-        Validates that deactivated is called during construction.
-        """
-        go = GameObjectSubclass()
-        go.handlers.validate(activate=go, activate_count=1)
+def test_deactivated_in_constructor():
+    """
+    Validates that deactivated is called during construction.
+    """
+    go = GameObjectSubclass()
+    go.handlers.validate(activate=go, activate_count=1)
 
-    def test_draw_handler_called(self):
-        """
-        This is a basic test that ensures the draw handler is called.
-        """
-        go = GameObjectSubclass()
 
-        go.handlers.validate(activate=go, activate_count=1)
-        go.handlers.reset()
+def test_draw_handler_called():
+    """
+    This is a basic test that ensures the draw handler is called.
+    """
+    go = GameObjectSubclass()
 
-        go.draw_hierarchy("surface")
-        go.handlers.validate(draw=(go, "surface"), draw_count=1)
+    go.handlers.validate(activate=go, activate_count=1)
+    go.handlers.reset()
 
-    def test_update_handler_called(self):
-        """
-        This is a basic test that ensures the update handler is called.
-        """
-        go = GameObjectSubclass()
+    go.draw_hierarchy("surface")
+    go.handlers.validate(draw=(go, "surface"), draw_count=1)
 
-        go.handlers.validate(activate=go, activate_count=1)
-        go.handlers.reset()
 
-        go.update_hierarchy(0.1)
-        go.handlers.validate(update=(go, 0.1), update_count=1)
+def test_update_handler_called():
+    """
+    This is a basic test that ensures the update handler is called.
+    """
+    go = GameObjectSubclass()
 
-    def test_destroy_handler_called(self):
-        """
-        This is a basic test that ensures the destroy handler is called.
-        """
-        go = GameObjectSubclass()
-        go.handlers.validate(activate=go, activate_count=1)
-        go.handlers.reset()
+    go.handlers.validate(activate=go, activate_count=1)
+    go.handlers.reset()
 
-        go.destroy()  # This will also deactivate the GameObject
-        go.handlers.validate(deactivate=go, deactivate_count=1, destroy=go, destroy_count=1,
-                             called_order=["deactivate", "destroy"])
+    go.update_hierarchy(0.1)
+    go.handlers.validate(update=(go, 0.1), update_count=1)
 
-    def test_activate_draw_update_deactivate_destroyed_handlers_called(self):
-        """
-        This is a basic test that ensures all the handlers are called.
-        """
-        go = GameObjectSubclass()
 
-        go.handlers.validate(activate=go, activate_count=1)
+def test_destroy_handler_called():
+    """
+    This is a basic test that ensures the destroy handler is called.
+    """
+    go = GameObjectSubclass()
+    go.handlers.validate(activate=go, activate_count=1)
+    go.handlers.reset()
 
-        go.handlers.reset()
-        go.draw_hierarchy("surface")
-        go.handlers.validate(draw=(go, "surface"), draw_count=1)
+    go.destroy()  # This will also deactivate the GameObject
+    go.handlers.validate(deactivate=go, deactivate_count=1, destroy=go, destroy_count=1,
+                         called_order=["deactivate", "destroy"])
 
-        go.handlers.reset()
-        go.update_hierarchy(0.1)
-        go.handlers.validate(update=(go, 0.1), update_count=1)
 
-        go.handlers.reset()
-        go.destroy()  # This will also deactivate the GameObject
-        go.handlers.validate(deactivate=go, deactivate_count=1, destroy=go, destroy_count=1,
-                             called_order=["deactivate", "destroy"])
+def test_activate_draw_update_deactivate_destroyed_handlers_called():
+    """
+    This is a basic test that ensures all the handlers are called.
+    """
+    go = GameObjectSubclass()
+
+    go.handlers.validate(activate=go, activate_count=1)
+
+    go.handlers.reset()
+    go.draw_hierarchy("surface")
+    go.handlers.validate(draw=(go, "surface"), draw_count=1)
+
+    go.handlers.reset()
+    go.update_hierarchy(0.1)
+    go.handlers.validate(update=(go, 0.1), update_count=1)
+
+    go.handlers.reset()
+    go.destroy()  # This will also deactivate the GameObject
+    go.handlers.validate(deactivate=go, deactivate_count=1, destroy=go, destroy_count=1,
+                         called_order=["deactivate", "destroy"])

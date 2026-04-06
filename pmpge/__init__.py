3321603cfab693441f0cfeb61c078b2ab4bf1089
Daniel Bloy <daniel_bloy@hotmail.com>
2026-04-06 21:29:13 +0100

    Fix structural issues

--- pmpge/__init__.py
+++ pmpge/__init__.py
@@ -3,10 +3,4 @@ import pmpge.environment as environment
 # Initialise the device next to allow it to perform any setup.
 __device = environment.import_driver('device')
 
-# Now we initialise each of the other subsystems.
-environment.import_driver('platform')
-environment.import_driver('graphics')
-environment.import_driver('controller')
-environment.import_driver('sound')
-
 environment.report()

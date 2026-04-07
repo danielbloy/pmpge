94c5f15f6dd66b9fb3268e30440dce9ce3e7b2f8
Daniel Bloy <daniel_bloy@hotmail.com>
2026-04-07 09:03:58 +0100

    More rework

--- pmpge/drivers/graphics/circuitpython.py
+++
@@ -1,3 +0,0 @@
-# TODO: Default driver is to use displayio
-# https://docs.circuitpython.org/en/latest/shared-bindings/displayio/
-# https://learn.adafruit.com/circuitpython-display-support-using-displayio/introduction
--- pmpge/drivers/graphics/micropython.py
+++
@@ -1,2 +0,0 @@
-# TODO: Default driver is to use pico-graphics:
-# https://github.com/pimoroni/pimoroni-pico/blob/main/micropython/modules/picographics/README.md

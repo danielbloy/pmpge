09ee6c12ded5816b29c9fdc0cb533ac01f34b044
Daniel Bloy <daniel_bloy@hotmail.com>
2026-04-11 13:34:06 +0100

    Added tests for lifetime

--- pmpge/traits/lifetime.py
+++ pmpge/traits/lifetime.py
@@ -4,8 +4,9 @@ class Lifetime:
     is infinite. Once set to a number, lifetime begins to count down to zero at which point it
     will automatically mark itself for destruction. You can keep a GameObject alive for longer
     by adjusting the value of lifetime.
+
+    Lifetime only works as a Trait of GameObject as it requires a destroy() method.
     """
-    destroy: bool
     lifetime: float | None
 
     def __init__(self, lifetime: float = None):
@@ -15,5 +16,5 @@ class Lifetime:
         if self.lifetime:
             self.lifetime -= dt
             if self.lifetime <= 0:
-                self.destroy = True
-                return
+                # noinspection PyUnresolvedReferences
+                self.destroy()
---
+++ tests/pmpge/traits/lifetime/test_lifetime.py
@@ -0,0 +1,122 @@
+import pytest
+
+from pmpge.game_object import GameObject
+from pmpge.traits.lifetime import Lifetime
+
+
+def test_constructor():
+    """
+    Simple test to ensure that Lifetime works.
+    """
+    trait = Lifetime()
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime is None
+
+    trait = Lifetime(10)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 10
+
+
+def test_update_when_no_lifetime_set():
+    """
+    Validates that destroy is not set when no lifetime set.
+    """
+    trait = Lifetime()
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime is None
+
+    trait.update(1)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime is None
+
+    trait.update(0)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime is None
+
+    trait.update(2)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime is None
+
+    # Now test when used as a GameObject
+    go = GameObject(Lifetime())
+    assert trait.lifetime is None
+    go.update_hierarchy(1)
+    assert trait.lifetime is None
+
+
+# noinspection PyUnresolvedReferences
+def test_update_when_lifetime_set():
+    """
+    Validates that lifetime decreased when lifetime set and triggers destroy.
+    """
+    trait = Lifetime(5)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 5
+
+    trait.update(1)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 4
+
+    trait.update(1)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 3
+
+    trait.update(2)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 1
+
+    trait.update(0)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 1
+
+    trait.update(0.5)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 0.5
+
+    # This will error because it is not part of a GameObject
+    with pytest.raises(AttributeError):
+        trait.update(0.5)
+
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 0
+
+    # Now test when used as a GameObject
+    go = GameObject(Lifetime(2))
+    assert go.lifetime == 2
+    assert go.alive
+    go.update_hierarchy(1)
+    assert go.lifetime == 1
+    assert go.alive
+    go.update_hierarchy(1)
+    assert go.lifetime == 0
+    assert not go.alive
+
+
+def test_update_when_extending_lifetime():
+    """
+    Validates that lifetime can be extended.
+    """
+    trait = Lifetime(5)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 5
+
+    trait.update(1)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 4
+
+    trait.lifetime = 6
+    trait.update(1)
+    assert not hasattr(trait, 'destroy')
+    assert trait.lifetime == 5
+
+    # Now test when used as a GameObject
+    go = GameObject(Lifetime(2))
+    assert go.lifetime == 2
+    assert go.alive
+    go.update_hierarchy(1)
+    assert go.lifetime == 1
+    assert go.alive
+    go.lifetime = 6
+    go.update_hierarchy(1)
+    assert go.lifetime == 5
+    assert go.alive

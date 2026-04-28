bb191f8c66f0c29a087d455a86a99c8b66e728fc
Daniel Bloy <daniel_bloy@hotmail.com>
2026-04-28 08:20:11 +0100

    Add another test

--- tests/validate_device/validate_all.py
+++ tests/validate_device/validate_all.py
@@ -7,9 +7,10 @@ import tests.validate_device.validate_d_traits as d
 import tests.validate_device.validate_d_traits_simple as ds
 import tests.validate_device.validate_e_graphics_multiple_sprites as ems
 import tests.validate_device.validate_e_graphics_single_sprite as ess
+import tests.validate_device.validate_e_graphics_sprite_movement as esm
 import tests.validate_device.validate_e_graphics_sprite_visibility as esv
 
-modules = [a, b, c, ch, ds, d, ess, ems, esv]
+modules = [a, b, c, ch, ds, d, ess, ems, esm, esv]
 
 
 def execute():
@@ -28,3 +29,5 @@ def execute():
 
 if utils.should_execute(__name__):
     execute()
+
+# TODO: Add a test to validate display Z-order

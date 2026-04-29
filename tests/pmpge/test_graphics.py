c0837718ca4750caaa2439c3411de4d47d5794a5
Daniel Bloy <daniel_bloy@hotmail.com>
2026-04-29 08:57:34 +0100

    Added some notes

--- tests/pmpge/test_graphics.py
+++ tests/pmpge/test_graphics.py
@@ -9,3 +9,19 @@
 # TODO: Need a build() render_pipeline - basically draw_hierarchy()
 #       returning a list of objects to render
 # TODO: Need a rebuild() render_pipeline
+
+
+# TODO: A GameObject needs:
+# * A Renderer(). The Renderer does not necessarily need to be an instance per GameObject
+# * A resource. This is easy for images and they can be shared.
+# * The Renderer needs access to GameObject() to get things like x, y co-ordinates
+# * The renderer should be updated(), certainly notified if something changes, though
+#   that is highly likely most of the time.
+#
+# * The information the renderer will need is:
+#   x, y, visible, asset/resource
+#
+# * The render pipeline needs to know when parent/child relationships change as this
+#   affects the order within which the objects are drawn.
+# * The render pipeline needs to know when objects are created/destroyed as the
+#   corresponding objects in the render pipeline will need to be added or removed.

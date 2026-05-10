bb4646dd26e3ec017f0cfad7c5f586b383da9e24
Daniel Bloy <daniel_bloy@hotmail.com>
2026-05-10 21:46:55 +0100

    Displayio scaling (#27)

--- pmpge/drivers/graphics/displayio.py
+++ pmpge/drivers/graphics/displayio.py
@@ -15,7 +15,7 @@
 # instances which are used to contain the graphics are constructed. When the
 # game is initialise (init() is called) we traverse the entire hierarchy (all
 # active AND inactive GameObjects) and generate the TileGrid instances. These
-# are all added to the root Group instance. Because we add them in the order
+# are all added to the object Group instance. Because we add them in the order
 # that we traverse the hierarchy, the implicit draw order is preserved - even
 # with the deactivated objects. We also only generate the TileGrid instances
 # we need. See the note below about why we don't minic the hierarchy structure.
@@ -69,23 +69,34 @@ import board
 
 # noinspection PyUnresolvedReferences,PyPackageRequirements
 from displayio import Group, Palette, Bitmap, TileGrid
+from pmpge.environment import calculate_scaling_factor
 from pmpge.game import Game
 from pmpge.game_object import GameObject, draw_hierarchy, traverse_hierarchy
-
-game: Game | None = None
 
 display = board.DISPLAY
 display.root_group = None
 display.brightness = 0.0  # Turn the display off until the game starts
 
+game: Game | None = None
+
 # Root group to place all items to draw.
 root = Group()
+background_group: Group = Group()
+object_group: Group = Group()
+border_group: Group = Group()
+root.append(background_group)
+root.append(object_group)
+root.append(border_group)
 
 # Create a single colour bitmap for the background.
 # Source: https://learn.adafruit.com/circuitpython-display-support-using-displayio/draw-pixels
 palette = Palette(1)
 palette[0] = 0x000000
 background = TileGrid(Bitmap(display.width, display.height, 1), pixel_shader=palette)
+background_group.append(background)
+
+border_palette = Palette(1)
+border_palette[0] = 0x000000
 
 
 def init(g: Game, sw: int, sh: int, bgc: tuple[int, int, int]):
@@ -96,10 +107,11 @@ def init(g: Game, sw: int, sh: int, bgc: tuple[int, int, int]):
     # FUTURE: When the game is smaller than the display and we don't scale, we need to add a border.
     #         The most common is 160 x 120 on a 160 x 128 display.
 
-    # FUTURE: We need to sort out scaling at some point. This can be done by setting: `root.scale = 2`
-    #         See: https://learn.adafruit.com/circuitpython-display-support-using-displayio/group#group-scale-3162091
-    global game, root
+    global game
     game = g
+
+    # FUTURE: If the background colour is black we could probably avoid the background object
+    #         entirely to save RAM.
 
     # Set the single colour in the palette for our background to the desired background colour
     red = bgc[0] & 255
@@ -109,26 +121,52 @@ def init(g: Game, sw: int, sh: int, bgc: tuple[int, int, int]):
 
     # Here we build the entire graphics hierarchy in order to place the tileGrid
     # instances in the correct order.
-    root = Group()
     game_object_hierarchy_changed()
 
-    # Setting up the root here stops all the graphics from showing as they are loading.
+    scaling_factor = calculate_scaling_factor(display.width, display.height, g.width, g.height)
+    object_group.scale = scaling_factor
+
+    # Now generate the borders to crop the screen to the desired game area. This currently
+    # just puts the borders at the bottom of the screen and on the right of the screen.
+    # FUTURE: Spread the borders more evenly.
+    # TODO: The code to generate the borders could be extract out returning a list of tuples
+    #       containing: width, height, x, y
+    game_area_width = g.width * scaling_factor
+    game_area_height = g.height * scaling_factor
+    border_width = display.width - game_area_width
+    border_height = display.height - game_area_height
+
+    if border_height > 0:
+        bottom_border = TileGrid(Bitmap(display.width, border_height, 1), pixel_shader=border_palette)
+        bottom_border.y = game_area_height
+        border_group.append(bottom_border)
+
+    if border_width > 0:
+        right_border = TileGrid(Bitmap(border_width, display.height, 1), pixel_shader=border_palette)
+        right_border.x = game_area_width
+        border_group.append(right_border)
+
     display.root_group = root
     display.brightness = 1
 
 
 def deinit():
     """
-    Removes everything left from the root group.
+    Removes everything left from the object group.
     """
     global game
     game = None
 
-    # Remove all the items from the root group (most should
+    # Remove all the items from the object group (most should
     # have been removed via the GameObject.destroy() method.
     display.root_group = None
-    while len(root) > 0:
-        root.pop()
+    while len(object_group) > 0:
+        object_group.pop()
+
+    # Now remove the borders.
+    while len(border_group) > 0:
+        border = border_group.pop()
+        border.bitmap.deinit()
 
     clear_image_cache()
 
@@ -142,32 +180,30 @@ def draw(screen):
 
 
 # This global setting is used to force all GameObjects to re-add their displayio
-# objects back to the root Group. This is used when rebuilding the hierarchy.
-force_add_to_root = False
+# objects back to the object Group. This is used when rebuilding the hierarchy.
+force_add_tile_grids = False
 
 
 def game_object_hierarchy_changed():
     """
     This notifies us that the game has made changes to the GameObject hierarchy. We don't
     know what those changes are so we forcibly remove and re-add all displayio objects
-    to the root Group. This ensures that the draw order of the objects is correct.
+    to the object Group. This ensures that the draw order of the objects is correct.
 
     This is an expensive operation so use sparingly.
     """
-    global force_add_to_root
-    force_add_to_root = True
+    global force_add_tile_grids
+    force_add_tile_grids = True
 
-    while len(root) > 0:
-        root.pop()
-
-    root.append(background)  # Needs to be the first item.
+    while len(object_group) > 0:
+        object_group.pop()
 
     def forced_draw(go: GameObject, state):
         go._draw(None)
         return True, None
 
     traverse_hierarchy(game.root, forced_draw)
-    force_add_to_root = False
+    force_add_tile_grids = False
 
 
 # FUTURE: Do something better for caching. We could also extract out the code in deinit().
@@ -242,8 +278,8 @@ class GraphicsDrawImageTrait:
         Draws the image at the specified position, offset from the GameObjects position.
         """
         image = self.image
-        if image.new_image_loaded or force_add_to_root:
-            root.append(image.tile_grid)
+        if image.new_image_loaded or force_add_tile_grids:
+            object_group.append(image.tile_grid)
             image.new_image_loaded = False
 
         tile_grid = image.tile_grid
@@ -259,11 +295,11 @@ class GraphicsDrawImageTrait:
 
     def destroyed(self):
         """
-        We hide the TileGrid and then remove it from the root.
+        We hide the TileGrid and then remove it from the object group.
         """
         tile_grid = self.image.tile_grid
         tile_grid.hidden = True
 
-        # Only remove from root if it has previously been attached.
+        # Only remove from object group if it has previously been attached.
         if not self.image.new_image_loaded:
-            root.remove(tile_grid)
+            object_group.remove(tile_grid)
--- pmpge/environment.py
+++ pmpge/environment.py
@@ -230,6 +230,42 @@ def import_driver(module: str):
 
 
 ################################################################################
+# U T I L I T I E S
+################################################################################
+def calculate_scaling_factor(screen_width: int, screen_height: int, game_width: int, game_height: int) -> int:
+    """
+    TODO: Comments
+    """
+    if hasattr(config, 'GRAPHICS_SCALING'):
+        return config.GRAPHICS_SCALING
+
+    if game_width >= screen_width or game_height >= screen_height:
+        return 1
+
+    sx = screen_width // game_width
+    sy = screen_height // game_height
+
+    return min(sx, sy)
+
+
+# TODO: Implement where is smooths over quarter seconds, always a quarter second behind.
+# TODO: Move to a class
+fps_last_4_quarters: list[int] = [0, 0, 0, 0]
+fps_current_quarter: int
+fps_current_quarter_index: int = 0
+fps_next_quarter_tick: float = 0
+
+
+def calculate_fps() -> int:
+    """
+    TODO: Comments
+    """
+    global fps_current_quarter
+    fps_current_quarter += 1
+    return sum(fps_last_4_quarters)
+
+
+################################################################################
 # E X E C U T I O N
 ################################################################################
 

40f6617c8df05177e220a6a76fe857d02b783eea
Daniel Bloy <daniel_bloy@hotmail.com>
2026-05-16 10:29:50 +0100

    More movement example.

--- examples/4 - Sprites/b_circular_motion_sprites.py
+++ examples/4 - Sprites/b_circular_motion_sprites.py
@@ -1,6 +1,5 @@
 """
-This example demonstrates one sprite orbiting another sprite which is
-orbiting the centre of the screen.
+This example demonstrates sprites orbiting other sprites.
 """
 import math
 import time
@@ -23,18 +22,35 @@ def terminate(dt: float):
         game.terminate()
 
 
+# Add the earth sprite which orbits the centre
 earth = Sprite(
     100, 100,
     AngularMotion(GAME_WIDTH // 2, GAME_HEIGHT // 2, 80, math.pi / 4),
     DrawImage("earth.png"))
 game.add_child(earth)
 
+# Add two moons which orbit earth
 moon = Sprite(
-    100, 100,
+    0, 0,
     AngularRelativeToParent(),
     AngularMotion(0, 0, 20, math.pi),
     DrawImage("moon.png"))
 earth.add_child(moon)
+
+moon2 = Sprite(
+    0, 0,
+    AngularRelativeToParent(),
+    AngularMotion(0, 0, 40, -math.pi),
+    DrawImage("moon.png"))
+earth.add_child(moon2)
+
+# Add a third moon which orbits one of the moons.
+moon3 = Sprite(
+    0, 0,
+    AngularRelativeToParent(),
+    AngularMotion(0, 0, 10, -math.pi),
+    DrawImage("moon.png"))
+moon2.add_child(moon3)
 
 game.add_update_func(terminate)
 

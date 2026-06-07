b7200cfe4e629551f3b1f6cac8151236f5d4ea81
Daniel Bloy <daniel_bloy@hotmail.com>
2026-06-07 21:32:06 +0100

    Updated

--- validate/validate_graphics.py
+++ validate/validate_graphics.py
@@ -1,14 +1,14 @@
-import validate.graphics.borders as egb
-import validate.graphics.multiple_sprites as ems
-import validate.graphics.scaling as egs
-import validate.graphics.single_sprite as ess
-import validate.graphics.sprite_hierarchy as esv
-import validate.graphics.sprite_movement as esm
-import validate.graphics.sprite_orbiting as ego
-import validate.graphics.sprite_visibility as esh
+import validate.graphics.borders as b
+import validate.graphics.multiple_sprites as ms
+import validate.graphics.scaling as gs
+import validate.graphics.single_sprite as ss
+import validate.graphics.sprite_hierarchy as sh
+import validate.graphics.sprite_movement as sm
+import validate.graphics.sprite_orbiting as so
+import validate.graphics.sprite_visibility as sv
 import validate.utils as utils
 
-modules = [egb, egs, ess, ems, esm, esv, esh, ego]
+modules = [b, gs, ss, ms, sm, sv, sh, so]
 
 # TODO: Small screen, add a graphic in each corner.
 

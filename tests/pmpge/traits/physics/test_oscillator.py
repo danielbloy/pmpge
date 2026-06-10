e71b4e6387fc8a158c77a0a3c810b7ee4418fa0e
Daniel Bloy <daniel_bloy@hotmail.com>
2026-06-10 21:16:26 +0100

    Added more tests

--- tests/pmpge/traits/physics/test_oscillator.py
+++ tests/pmpge/traits/physics/test_oscillator.py
@@ -1,7 +1,9 @@
 import pytest
 
 from pmpge.game_object import GameObject, update_hierarchy
-from pmpge.traits.physics import HorizontalOscillator, VerticalOscillator
+from pmpge.sprite import Sprite
+from pmpge.traits.physics import Acceleration
+from pmpge.traits.physics import HorizontalOscillator, VerticalOscillator, Velocity
 
 
 def test_horizontal_oscillator_constructor():
@@ -30,7 +32,7 @@ def test_vertical_oscillator_constructor():
 
 def test_horizontal_oscillator_without_velocity():
     """
-    Validates a Velocity trait is required.
+    Validates a Accelerate trait is required.
     """
     go = GameObject(HorizontalOscillator(10, 20))
     with pytest.raises(AttributeError):
@@ -39,11 +41,96 @@ def test_horizontal_oscillator_without_velocity():
 
 def test_vertical_oscillator_without_velocity():
     """
-    Validates a Velocity trait is required.
+    Validates a Accelerate trait is required.
     """
     go = GameObject(VerticalOscillator(10, 20))
     with pytest.raises(AttributeError):
         update_hierarchy(go, 0)
 
-# TODO: Test HorizontalOscillator
-# TODO: Test VerticalOscillator
+
+def test_horizontal_oscillator_hit_max():
+    """
+    Some simple tests to ensure it oscillates at the limit.
+    """
+    go = Sprite(0, 0, Velocity(0, 0), Acceleration(10, 0), HorizontalOscillator(10, 20))
+    update_hierarchy(go, 1)
+    assert go.x == 0
+    assert go.y == 0
+    assert go.vx == 10
+    assert go.vy == 0
+    assert go.ax == 10
+    assert go.ay == 0
+
+    update_hierarchy(go, 2)
+    assert go.x == 20
+    assert go.y == 0
+    assert go.vx == 30
+    assert go.vy == 0
+    assert go.ax == 10
+    assert go.ay == 0
+
+    # The next update should result in a bounce
+    update_hierarchy(go, 0.1)
+    assert go.x == 23
+    assert go.y == 0
+    assert go.vx == 31
+    assert go.vy == 0
+    assert go.ax == -10
+    assert go.ay == 0
+
+    # Now go backwards
+    update_hierarchy(go, 2)
+    assert go.x == 85
+    assert go.y == 0
+    assert go.vx == 11
+    assert go.vy == 0
+    assert go.ax == -10
+    assert go.ay == 0
+
+    update_hierarchy(go, 0.2)
+    assert go.x == 87.2
+    assert go.y == 0
+    assert go.vx == 9
+    assert go.vy == 0
+    assert go.ax == -10
+    assert go.ay == 0
+
+
+def test_horizontal_oscillator_hit_min():
+    """
+    Some simple tests to ensure it oscillates at the limit.
+    """
+    go = Sprite(30, 0, Velocity(0, 0), Acceleration(-10, 0), HorizontalOscillator(10, 20))
+    update_hierarchy(go, 1)
+    assert go.x == 30
+    assert go.y == 0
+    assert go.vx == -10
+    assert go.vy == 0
+    assert go.ax == -10
+    assert go.ay == 0
+
+    update_hierarchy(go, 2)
+    assert go.x == 10
+    assert go.y == 0
+    assert go.vx == -30
+    assert go.vy == 0
+    assert go.ax == -10
+    assert go.ay == 0
+
+    # The next update should result in a bounce
+    update_hierarchy(go, 0.1)
+    assert go.x == 7
+    assert go.y == 0
+    assert go.vx == -31
+    assert go.vy == 0
+    assert go.ax == 10
+    assert go.ay == 0
+
+    # Now go backwards
+    update_hierarchy(go, 0.1)
+    assert go.x == 3.9
+    assert go.y == 0
+    assert go.vx == -30
+    assert go.vy == 0
+    assert go.ax == 10
+    assert go.ay == 0

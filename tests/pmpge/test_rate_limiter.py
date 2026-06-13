07e9b9a52fd46a2865f1a4d43a062992c5feba75
Daniel Bloy <daniel_bloy@hotmail.com>
2026-06-13 13:01:18 +0100

    Updated

--- tests/pmpge/test_rate_limiter.py
+++ tests/pmpge/test_rate_limiter.py
@@ -50,6 +50,102 @@ def test_always_fires_on_first_update():
     assert callback_count == 3
 
 
+def test_correctly_fires_first_and_second_events():
+    """
+    Validates that RateLimiter correctly fires the first and second events.
+    The first event always fires but the second only when the timer expires.
+    """
+    elapsed_time: float = 0.0
+    callback_count: int = 0
+
+    def callback(dt: float):
+        nonlocal elapsed_time, callback_count
+        elapsed_time = dt
+        callback_count += 1
+
+    rl = RateLimiter(callback, rate=5)
+    assert math.isclose(elapsed_time, 0.0)
+    assert callback_count == 0
+
+    # First call is at 0.1 seconds which will cause the first event to fire but
+    # has not triggered the second event. The elapsed time should equal the initial
+    # passed in value. We repeat this test multiple times with multiple values.
+    rl(0.0)
+    assert math.isclose(elapsed_time, 0.0)
+    assert callback_count == 1
+
+    rl = RateLimiter(callback, rate=5)
+    rl(0.01)
+    assert math.isclose(elapsed_time, 0.01)
+    assert callback_count == 2
+
+    rl = RateLimiter(callback, rate=5)
+    rl(0.1)
+    assert math.isclose(elapsed_time, 0.1)
+    assert callback_count == 3
+
+    rl = RateLimiter(callback, rate=5)
+    rl(0.15)
+    assert math.isclose(elapsed_time, 0.15)
+    assert callback_count == 4
+
+    # Now we go over to the second trigger. This is also repeated with different values
+    callback_count = 0
+    rl = RateLimiter(callback, rate=5)
+    rl(0.0)
+    assert math.isclose(elapsed_time, 0.0)
+    assert callback_count == 1
+
+    rl(0.2)
+    assert math.isclose(elapsed_time, 0.2)
+    assert callback_count == 2
+
+    callback_count = 0
+    rl = RateLimiter(callback, rate=5)
+    rl(0.1)
+    assert math.isclose(elapsed_time, 0.1)
+    assert callback_count == 1
+
+    rl(0.1)
+    assert math.isclose(elapsed_time, 0.1)
+    assert callback_count == 2
+
+    callback_count = 0
+    rl = RateLimiter(callback, rate=5)
+    rl(0.15)
+    assert math.isclose(elapsed_time, 0.15)
+    assert callback_count == 1
+
+    rl(0.1)
+    assert math.isclose(elapsed_time, 0.1)
+    assert callback_count == 2
+
+    # Now we test when the initial delay is so long it goes over the second event (the
+    # first event is the initial one that is always called). We do this using 2 scenarios
+    callback_count = 0
+    rl = RateLimiter(callback, rate=5)
+    rl(0.3)
+    assert math.isclose(elapsed_time, 0.3)
+    assert callback_count == 1
+
+    # At this point, the first event was 0.1 seconds over the second event so the second
+    # event will get fired, regardless of the time.
+    rl(0.0)
+    assert math.isclose(elapsed_time, 0.0)
+    assert callback_count == 2
+
+    # Scenario 2
+    callback_count = 0
+    rl = RateLimiter(callback, rate=5)
+    rl(0.3)
+    assert math.isclose(elapsed_time, 0.3)
+    assert callback_count == 1
+
+    rl(0.1)
+    assert math.isclose(elapsed_time, 0.1)
+    assert callback_count == 2
+
+
 def test_fires_at_correct_rate():
     """
     Validates that the callback is called at the rate required.
@@ -140,6 +236,91 @@ def test_fires_at_correct_rate():
     assert math.isclose(elapsed_time_5, 0.4)
     assert callback_count_5 == 3
 
-# TODO: More finese in timinng
-# TODO: Test rate limit does not fire until counter passed.
-# TODO: Test rate limit catches up.
+
+def test_correctly_rate_limits():
+    """
+    Validates that RateLimiter correctly limits the call rate.
+    """
+    elapsed_time: float = 0.0
+    callback_count: int = 0
+
+    def callback(dt: float):
+        nonlocal elapsed_time, callback_count
+        elapsed_time = dt
+        callback_count += 1
+
+    rl = RateLimiter(callback, rate=5)
+    assert math.isclose(elapsed_time, 0.0)
+    assert callback_count == 0
+
+    # First call is at 0.1 seconds which will cause the first event to fire
+    rl(0.1)
+    assert math.isclose(elapsed_time, 0.1)
+    assert callback_count == 1
+
+    # None of these will trigger the second event, which will not occur until 0.2 seconds
+    # have passed.
+    rl(0.0)
+    rl(0.02)
+    rl(0.05)
+    assert math.isclose(elapsed_time, 0.1)
+    assert callback_count == 1
+
+    # Now we trip the second event.
+    rl(0.04)
+    assert math.isclose(elapsed_time, 0.11)
+    assert callback_count == 2
+
+    # Now we trip the third event which should be 0.19 seconds later.
+    rl(0.08)
+    rl(0.05)
+    rl(0.06)
+    assert math.isclose(elapsed_time, 0.19)
+    assert callback_count == 3
+
+
+def test_when_cant_keep_up():
+    """
+    Validates the RateLimiter when the gaps are so large the desired
+    rate cannot be achieved.
+    """
+    elapsed_time: float = 0.0
+    callback_count: int = 0
+
+    def callback(dt: float):
+        nonlocal elapsed_time, callback_count
+        elapsed_time = dt
+        callback_count += 1
+
+    rl = RateLimiter(callback, rate=5)
+    assert math.isclose(elapsed_time, 0.0)
+    assert callback_count == 0
+
+    # Each call will be so far apart that each invocation will result in a trigger
+    rl(0.3)
+    assert math.isclose(elapsed_time, 0.3)
+    assert callback_count == 1
+
+    rl(0.4)
+    assert math.isclose(elapsed_time, 0.4)
+    assert callback_count == 2
+
+    rl(0.5)
+    assert math.isclose(elapsed_time, 0.5)
+    assert callback_count == 3
+
+    rl(0.3)
+    assert math.isclose(elapsed_time, 0.3)
+    assert callback_count == 4
+
+    # Catches up
+    rl(0.0)
+    assert math.isclose(elapsed_time, 0.0)
+    assert callback_count == 5
+
+    # But does not keep triggering
+    rl(0.0)
+    rl(0.1)
+    rl(0.0)
+    assert math.isclose(elapsed_time, 0.0)
+    assert callback_count == 5

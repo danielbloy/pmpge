a3d747c5a75535eb90ad40abcde324cfaaecfe27
Daniel Bloy <daniel_bloy@hotmail.com>
2026-06-14 17:21:49 +0100

    Fixed up support for direct pin controller

--- devices/CircuitPython/config.py-edge_badge.py
+++ devices/CircuitPython/config.py-edge_badge.py
@@ -1,4 +1,4 @@
 # Settings for the shift register controller
 from board import BUTTON_CLOCK, BUTTON_OUT, BUTTON_LATCH
 
-BUTTON_MAPPING = [7, 6, 0, 1, 3, 5, 4, 2]
+CONTROLLER_MAPPING = [7, 6, 0, 1, 3, 5, 4, 2]

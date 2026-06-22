d4152a49a764b023db0a46bcd9e21c8e05e1be00
Daniel Bloy <daniel_bloy@hotmail.com>
2026-06-22 08:45:28 +0100

    Rename

--- validate/validate_graphics.py
+++ validate/validate_graphics.py
@@ -1,14 +1,14 @@
 import validate.graphics.borders as borders
 import validate.graphics.scaling as scaling
-import validate.graphics.sprite_hierarchy as hierarchy
 import validate.graphics.sprites as sprites
 import validate.graphics.visibility as visibility
+import validate.graphics.z_order as z_order
 import validate.utils as utils
 
 # TODO: Add a test on transparency
 
-modules = [borders, scaling, sprites, visibility, hierarchy]
-modules = [hierarchy]
+modules = [borders, scaling, sprites, visibility, z_order]
+modules = [z_order]
 
 if utils.should_execute(__name__):
     utils.execute_modules(modules)
